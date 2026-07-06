309a2b3508efa35bfc9f38c52dacc3c38d9ef16a
JohnTravolta <johntravolta@192.168.1.16>
2026-07-06 19:46:39 +0200

    Add BMW G26 Seria 4 Gran Coupe 2021-2023 pre-facelift

--- otomoto_tracker.py
+++ otomoto_tracker.py
@@ -86,6 +86,23 @@ SEARCHES = [
         "only_damaged": True,
         "olx_query": "bmw seria 3 g20 diesel xdrive sedan",
     },
+    {
+        "name": "BMW G26 Seria 4 Gran Coupe 2.0d xDrive AT 2021-2023",
+        "url": (
+            "https://www.otomoto.pl/osobowe/bmw/seria-4"
+            "?search%5Bfilter_enum_fuel_type%5D=diesel"
+            "&search%5Bfilter_enum_gearbox%5D=automatic"
+            "&search%5Bfilter_enum_drive%5D=awd"
+            "&search%5Bfilter_float_year%3Afrom%5D=2021"
+            "&search%5Bfilter_float_year%3Ato%5D=2023"
+            "&search%5Bfilter_float_engine_capacity%3Afrom%5D=1900"
+            "&search%5Bfilter_float_engine_capacity%3Ato%5D=2100"
+            "&search%5Bfilter_enum_bodywork_type%5D=coupe"
+        ),
+        "require_model_contains": None,
+        "only_damaged": True,
+        "olx_query": "bmw seria 4 gran coupe g26 diesel xdrive",
+    },
 ]
 
 # Słowa sugerujące uszkodzenie / wypadek
